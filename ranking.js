07f1807bf04ad77d6ef5d1ef8eef7f4360b4dfb1
schormeikerlugo <schormeikerl@gmail.com>
2025-05-24 18:01:08 -0400

    Optomizacion general

--- ranking.js
+++ ranking.js
@@ -1,56 +1,6 @@
 const wrapper = document.querySelector('.ratings-wrapper');
-let isDragging = false;
-let startX, scrollLeft;
 let autoScrollInterval;
 let waitingToRestart = false;
-
-// Mouse drag (mejorado)
-wrapper.addEventListener('mousedown', (e) => {
-  isDragging = true;
-  startX = e.pageX - wrapper.offsetLeft;
-  scrollLeft = wrapper.scrollLeft;
-  wrapper.style.cursor = 'grabbing';
-  stopAutoScroll();
-});
-
-document.addEventListener('mousemove', (e) => {
-  if (!isDragging) return;
-  e.preventDefault();
-  const x = e.pageX - wrapper.offsetLeft;
-  const walk = (x - startX) * 1.5;
-  wrapper.scrollLeft = scrollLeft - walk;
-});
-
-document.addEventListener('mouseup', () => {
-  if (isDragging) {
-    isDragging = false;
-    wrapper.style.cursor = 'grab';
-    startAutoScroll();
-  }
-});
-
-// Touch drag (mejorado)
-wrapper.addEventListener('touchstart', (e) => {
-  isDragging = true;
-  startX = e.touches[0].pageX - wrapper.offsetLeft;
-  scrollLeft = wrapper.scrollLeft;
-  stopAutoScroll();
-}, { passive: true });
-
-document.addEventListener('touchmove', (e) => {
-  if (!isDragging) return;
-  const touch = e.touches[0];
-  const x = touch.pageX - wrapper.offsetLeft;
-  const walk = (x - startX) * 1.5;
-  wrapper.scrollLeft = scrollLeft - walk;
-}, { passive: false });
-
-document.addEventListener('touchend', () => {
-  if (isDragging) {
-    isDragging = false;
-    startAutoScroll();
-  }
-});
 
 function startAutoScroll() {
   if (autoScrollInterval || waitingToRestart) return;
